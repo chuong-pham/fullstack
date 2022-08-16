a27e19e5eff69c36f5c0466dfc42d22969e3ff55
Chuong Pham <slim.james.pham@gmail.com>
2022-08-15 23:27:42 -0500

    added some demos

--- js/script.js
+++ js/script.js
@@ -1,32 +1,14 @@
-function clunk(times) {
-  var num = times;
-  while (num > 0) {
-    display("clunk");
-    num--;
-  }
+let car = {
+  make: "toyota",
+  model: "camry",
+  year: 2006,
+  wheels: 4
+};
+
+delete car.wheels;
+console.log(car);
+
+for(let prop in car) {
+  console.log(prop + ": " + car[prop]);
 }
 
-function thingamajig(size) {
-  var facky = 1;
-  clunkCounter = 0;
-  if (size == 0) {
-    display("clank");
-  } else if (size == 1) {
-    display("thunk");
-  } else {
-    while (size > 1) {
-      facky *= size;
-      size--;
-    }
-    clunk(facky);
-  }
-}
-
-function display(output) {
-  console.log(output);
-  clunkCounter++;
-}
-
-var clunkCounter = 0;
-thingamajig(0);
-console.log(clunkCounter);
